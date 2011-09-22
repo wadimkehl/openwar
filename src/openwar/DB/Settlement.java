8d1e3522bab5b31b86b08b49e8f66eee62efc6c7
wadimkehl <wadimkehl@3dceeaef-d162-4c3f-cb8d-46382f05b9d6>
2011-09-22 16:30:04 +0000

    refactor, fixes and banners

--- src/openwar/DB/Settlement.java
+++ src/openwar/DB/Settlement.java
@@ -11,13 +11,18 @@ import com.jme3.font.Rectangle;
 import com.jme3.material.Material;
 import com.jme3.material.RenderState.BlendMode;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.queue.RenderQueue.Bucket;
+import com.jme3.renderer.queue.RenderQueue.ShadowMode;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.control.BillboardControl;
+import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Quad;
 import java.util.ArrayList;
 import openwar.Main;
+import openwar.world.WorldMap;
 
 /**
  *
@@ -26,25 +31,53 @@ import openwar.Main;
 public class Settlement {
 
     public String name;
+    public String region;
     public int level, population, posX, posZ;
     public ArrayList<Building> buildings;
     public ArrayList<Unit> units;
     public Spatial model;
-    public Geometry billBoard;
+    public Spatial billBoard;
+    public Spatial banner;
+    public Node node;
+    public WorldMap map;
 
     public Settlement() {
         buildings = new ArrayList<Building>();
         units = new ArrayList<Unit>();
-
-
-
+        node = new Node();
     }
 
-    public void createBillBoard(Main game) {
+    public void createData(WorldMap m) {
+        map = m;
+
+        //Spatial m = Main.DB.genBuildings.get("city").levels.get(0).model.clone();
+        model = (Spatial) new Geometry("city", new Box(Vector3f.ZERO, 1.2f, 0.25f, 1.2f));
+        Material mat = new Material(map.game.getAssetManager(), "Common/MatDefs/Light/Lighting.j3md");
+        model.setMaterial(mat);
+        model.setShadowMode(ShadowMode.CastAndReceive);
+        model.setLocalTranslation(0f,0.25f,0f);
+        node.attachChild(model);
+
+        banner = (Spatial) new Geometry("", new Quad(1f, 2f));
+        banner.setLocalTranslation(-0.5f, 1f, 0);
+        mat = new Material(map.game.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
+        mat.setTexture("ColorMap", Main.DB.genFactions.get(Main.DB.hashedRegions.get(region).owner).banner);
+        banner.setQueueBucket(Bucket.Translucent);
+        banner.setMaterial(mat);
+        node.attachChild(banner);
+
+        node.setLocalTranslation(map.getGLTileCenter(posX, posZ));
+        map.scene.attachChild(node);
+
+        createBillBoard();
+    }
+
+    public void createBillBoard() {
 
 
 
-        BitmapFont fnt = game.getAssetManager().loadFont("ui/fonts/palatino.fnt");
+        BitmapFont fnt = map.game.getAssetManager().loadFont("ui/fonts/palatino.fnt");
         BitmapText label = new BitmapText(fnt, false);
         label.setSize(1f);
         label.setText(name);
@@ -54,32 +87,25 @@ public class Settlement {
         label.setColor(ColorRGBA.Black);
         label.setQueueBucket(Bucket.Translucent);
 
-        label.setLocalTranslation(posX - 1 + 0.25f, 5-height/2, posZ + 0.0001f);
+        label.setLocalTranslation(- 1.25f , 7 - height / 2, 0.0001f);
         label.addControl(new BillboardControl());
-        game.worldMapState.map.scene.attachChild(label);
+        node.attachChild(label);
 
-        billBoard = new Geometry(name + "_billboard", new Quad(width+0.5f, height+0.5f, false));
-        Material mat = new Material(game.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+        billBoard = new Geometry(name + "_billboard", new Quad(width + 0.5f, height + 0.5f, false));
+        Material mat = new Material(map.game.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
         mat.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
 //        mat.setTexture("ColorMap",game.getAssetManager().loadTexture("Textures/ColoredTex/Monkey.png"));
-        mat.setTransparent(true);
         billBoard.setMaterial(mat);
         billBoard.setQueueBucket(Bucket.Transparent);
         billBoard.addControl(new BillboardControl());
-        billBoard.setLocalTranslation(posX - 1f, 3, posZ);
+        billBoard.setLocalTranslation(- 1.5f, 5, 0);
 
-        game.worldMapState.map.scene.attachChild(billBoard);
+        node.attachChild(billBoard);
 
 
 
     }
 
-    public void updateBillBoard(Main game) {
-
-        if (billBoard == null) {
-            createBillBoard(game);
-        }
-
-
+    public void update() {
     }
 }
